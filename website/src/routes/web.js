77c5c219f18308834ded9f5ee7ee8f2868fca59b
delficasarino <delficasarino@gmail.com>
2021-06-24 13:42:57 -0300

    actualizacion de web.js

--- website/src/routes/web.js
+++ website/src/routes/web.js
@@ -5,5 +5,11 @@ const path = require('path');
 
 //Website Routes
 app.get("/", (req,res) => res.sendFile(path.resolve(__dirname, "../views/index.html")));
+app.get("/almacenProductDetail", (req,res) => res.sendFile(path.resolve(__dirname, "../views/almacenProductDetail.html")));
+app.get("/gourmetProductDetail", (req,res) => res.sendFile(path.resolve(__dirname, "../views/gourmetProductDetail.html")));
+app.get("/cosmeticaProductDetail", (req,res) => res.sendFile(path.resolve(__dirname, "../views/cosmeticaProductDetail.html")));
+app.get("/productCart", (req,res) => res.sendFile(path.resolve(__dirname, "../views/productCart.html")));
+app.get("/register", (req,res) => res.sendFile(path.resolve(__dirname, "../views/register.html")));
+app.get("/login", (req,res) => res.sendFile(path.resolve(__dirname, "../views/login.html")));
 
 module.exports = app;
