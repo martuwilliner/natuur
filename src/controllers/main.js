561b778bd5045175d16d4a681222f5c515bea0ad
delficasarino <delficasarino@gmail.com>
2021-08-19 11:26:11 -0300

    slice en destacados

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -4,7 +4,7 @@ const mainController = {
     index: (req,res) => {
         return res.render('index', 
         {products: 
-            product.allWithExtra().filter(product => product.oferts == true), 
+            product.allWithExtra().filter(product => product.oferts == true).slice(-4), 
         styles: ["/css/main-index.css"],
         title: "Natuur | Bienvenidos"
         }
