f5b264e85c177df7e3bc0c55541a41bf46d70a5b
IamRoman-rb <roman.borla2004@mail.com>
2021-09-22 23:22:47 -0300

    Creando validaciones front

--- public/js/login.js
+++ public/js/login.js
@@ -4,18 +4,10 @@ form.addEventListener("submit", e => {
     e.preventDefault();
     let errors = [];
     const errorForm = document.querySelector("#errores-form");
-    const nombre = document.querySelector("#nombre");
-    const apellido = document.querySelector("#apellido");
     const email = document.querySelector("#email");
     const password = document.querySelector("#password");
     const regexEmail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
     const regexPassword = /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/
-    if (nombre.value.length < 2) {
-        errors.push("El nombre deberia ser mas largo");
-    }
-    if (apellido.value.length < 2) {
-        errors.push("El apellido deberia ser mas largo");
-    }
     if (!regexEmail.test(email.value)) {
         errors.push("Tenes que agregar mas caracteres");
     }
