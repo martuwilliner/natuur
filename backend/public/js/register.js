a5cb3107637ed8df27aadf640a4641310ff37686
delficasarino <delficasarino@gmail.com>
2021-10-19 15:46:29 -0300

    ultimos cambios css y boton al dashboard

--- backend/public/js/register.js
+++ backend/public/js/register.js
@@ -11,16 +11,16 @@ form.addEventListener("submit", e => {
     const regexEmail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
     const regexPassword = /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/
     if (nombre.value.length < 2) {
-        errors.push("El nombre deberia ser mas largo");
+        errors.push("El nombre debe ser más largo");
     }
     if (apellido.value.length < 2) {
-        errors.push("El apellido deberia ser mas largo");
+        errors.push("El apellido debe ser más largo");
     }
     if (!regexEmail.test(email.value)) {
-        errors.push("Tiene que ser un email");
+        errors.push("Tiene que ser un email válido");
     }
     if (!regexPassword.test(password.value)) {
-        errors.push("Tu contraseña no es segura");
+        errors.push("Tu contraseña debe tener al menos una mayus y un caracter especial");
     }
     if (errors.length > 0) {
         errorForm.innerHTML = null;
